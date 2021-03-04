2d28912d61e06afc24aecbf29d40798d7b732b58
AnastasiiaSergeeva <73837165+AnastasiiaSergeeva@users.noreply.github.com>
2021-03-04 22:55:36 +0200

    8

--- modul-4.js/task-7.js
+++ modul-4.js/task-7.js
@@ -20,3 +20,20 @@ composeMessage.call(orders);
 const messages = orders.map(
     (order, index) => composeMessage.call(order, index)
 );
+
+==============
+
+const orders = [
+  { email: 'solomon@topmail.ua', dish: 'Burger' },
+  { email: 'artemis@coldmail.net', dish: 'Pizza' },
+  { email: 'jacob@mail.com', dish: 'Taco' },
+];
+
+// Пиши код ниже этой строки
+function composeMessage(position) {
+  return `Готовим ${this.dish} для ${this.email}. Ваш заказ ${position}-й в очереди.`;
+}
+
+const messages = orders.map((order, index) =>
+  composeMessage.call(order, index + 1)
+);
